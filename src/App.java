e1c0fdee74e946d4d0a608df37b63b1bd793e873
fcmfreitas <franciscocmfreitas@gmail.com>
2024-08-21 12:29:56 -0300

    exercicio 3

--- src/App.java
+++ src/App.java
@@ -1,18 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        Carro suv = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
 
         System.out.println("Tipos de veiculos:");
-        System.out.println(utilitario);
+        System.out.println(suv);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
-        utilitario.abastece(TipoCombustivel.DIESEL, 70);
-        System.out.println(utilitario);
+        suv.abastece(TipoCombustivel.GASOLINA, 55);
+        System.out.println(suv);
         System.out.println("\nViajando com o carro basico");
-        utilitario.viaja(250);
-        utilitario.viaja(150);
-        System.out.println(utilitario);
+        suv.viaja(250);
+        suv.viaja(150);
+        System.out.println(suv);
     }
 }
